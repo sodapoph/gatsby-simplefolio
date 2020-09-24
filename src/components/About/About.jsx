e82801882e6dd53f59663d634d237989e8d1c9b7
sodapoph <sodapoph@gmail.com>
2020-09-24 17:00:15 -0500

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Fade from 'react-reveal/Fade';
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title';
+import AboutImg from '../Image/AboutImg';
+import PortfolioContext from '../../context/context';
+
+const About = () => {
+  const { about } = useContext(PortfolioContext);
+  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="about">
+      <Container>
+        <Title title="About Me" />
+        <Row className="about-wrapper">
+          <Col md={6} sm={12}>
+            <Fade bottom duration={1000} delay={600} distance="30px">
+              <div className="about-wrapper__image">
+                <AboutImg alt="profile picture" filename={img} />
+              </div>
+            </Fade>
+          </Col>
+          <Col md={6} sm={12}>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <div className="about-wrapper__info">
+                <p className="about-wrapper__info-text">
+                  {paragraphOne ||
+                    'Soraida is a web developer and multimedia artist based out of Minneapolis, Minnesota who integrates a love for illustration with ever-changing technologies.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphTwo ||
+                    'She is a graduate of Minneapolis College of Art and Design with a BFA in Web and Multimedia Environments. She recently attended Le Wagon Tokyo, a 9-week intensive coding bootcamp to learn HTML, CSS, Bootstrap, JavaScript ES2015, SQL, git, GitHub, Heroku and Ruby on Rails.'}
+                </p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree || 'In her spare time, Soraida likes to sing karaoke, eat good food, and see the world through her thick glasses.'}
+                </p>
+                {resume && (
+                  <span className="d-flex mt-3">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--resume"
+                      href={resume}
+                    >
+                      Resume
+                    </a>
+                  </span>
+                )}
+              </div>
+            </Fade>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/mock/data.js
@@ -0,0 +1,94 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: '', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: '',
+  subtitle: '',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: '',
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'chicken-cover.png',
+    title: 'Chicken Coop',
+    info: 'Mock-ups for a mobile app pitch. Chicken Coop is an app that helps users find local farms and agriculture hobbyists who sell eggs, dairy, and honey.',
+    info2: 'Created with Figma',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'cc-banner.png',
+    title: 'CleverCloset',
+    info: `A mobile application dedicated to reducing one's impact on the environment by managing the clothes the user may already own, and encouraging the user to donate to charity shops or share their clothing items among friends. This project was developed in two weeks in a team of four. I handled front-end development and the UI/UX.`,
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'project.jpg',
+    title: '',
+    info: '',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: '',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'twitter',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'codepen',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: '',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+};
